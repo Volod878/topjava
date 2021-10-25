17b73a6941a51546083e9ff385452c1ce9ca7422
Volod878 <volod1988@gmail.com>
2021-10-26 01:03:48 +0700

    HW03

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM meals;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES (TIMESTAMP '2021-10-25 10:00:00', 'Завтрак', 500, 100000),
+       (TIMESTAMP '2021-10-25 13:00:00', 'Обед', 1000, 100000),
+       (TIMESTAMP '2021-10-25 20:00:00', 'Ужин', 500, 100000),
+       (TIMESTAMP '2021-10-25 00:00:00', 'Еда на граничное значение', 1000, 100000),
+       (TIMESTAMP '2021-10-24 10:00:00', 'Завтрак', 1000, 100000),
+       (TIMESTAMP '2021-10-24 13:00:00', 'Обед', 500, 100000),
+       (TIMESTAMP '2021-10-24 20:00:00', 'Ужин', 410, 100000),
+
+       (TIMESTAMP '2021-9-15 14:00:00', 'Админ ланч', 510, 100001),
+       (TIMESTAMP '2021-9-15 21:00:00', 'Админ ужин', 1500, 100001);
